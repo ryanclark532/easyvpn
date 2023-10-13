7bc470b1b8b18300ad17463d125200ae15de3f97
Ryan Clark <42229549+ryanclark532@users.noreply.github.com>
2023-10-13 16:30:31 +1100

    Started chi migration

--- go.mod
+++ go.mod
@@ -11,15 +11,34 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute v1.23.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/Masterminds/squirrel v1.5.4 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dghubble/oauth1 v0.7.2 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-oauth2/oauth2/v4 v4.5.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-pkgz/auth v1.22.0 // indirect
+	github.com/go-pkgz/repeater v1.1.3 // indirect
+	github.com/go-pkgz/rest v1.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.26 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/nullrocks/identicon v0.0.0-20180626043057-7875f45b0022 // indirect
 	github.com/reiver/go-oi v1.0.0 // indirect
 	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
@@ -27,7 +46,21 @@ require (
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	github.com/ziutek/telnet v0.0.0-20180329124119-c3b780dc415b // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	go.etcd.io/bbolt v1.3.7 // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/image v0.13.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"easyvpn/src/auth"
 	"easyvpn/src/database"
 	"easyvpn/src/groups"
-	"easyvpn/src/middleware"
 	"easyvpn/src/user"
-	"easyvpn/src/utils"
 	"easyvpn/src/vpn"
+	"net/http"
+	"strings"
+	"time"
+
+	"easyvpn/src/utils"
 	"embed"
 	"fmt"
 
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-pkgz/auth"
+	"github.com/go-pkgz/auth/avatar"
+	"github.com/go-pkgz/auth/provider"
+	"github.com/go-pkgz/auth/token"
 )
 
 //go:embed app/*
@@ -48,7 +53,24 @@ func main() {
 		panic(vpnerr)
 	}
 
-	r := SetupRouter()
+	options := auth.Opts{
+		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
+			return "secret", nil
+		}),
+		TokenDuration:  time.Minute * 5, // token expires in 5 minutes
+		CookieDuration: time.Hour * 24,  // cookie expires in 1 day and will enforce re-login
+		Issuer:         "easy-vpn",
+		URL:            "http://localhost:8080",
+		AvatarStore:    avatar.NewLocalFS("/tmp"),
+		Validator: token.ValidatorFunc(func(_ string, claims token.Claims) bool {
+			// allow only dev_* names
+			return claims.User != nil && strings.HasPrefix(claims.User.Name, "dev_")
+		}),
+	}
+
+	service := auth.NewService(options)
+
+	r := setupRouter(service)
 
 	port := "8080"
 	fmt.Printf("Server is listening on port %s...\n", port)
@@ -59,33 +81,75 @@ func main() {
 	}
 }
 
-func SetupRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.Use(middleware.CorsMiddleware)
+/*
+	func SetupRouter() *mux.Router {
+		r := mux.NewRouter()
+		r.Use(middleware.CorsMiddleware)
 
-	apiRouter := r.PathPrefix("/api").Subrouter()
-	apiRouter.HandleFunc("/auth/sign-in", auth.UserLoginEndpoint).Methods(http.MethodPost, http.MethodOptions)
-	apiRouter.HandleFunc("/auth/check-token", auth.CheckUserTokenEndpoint).Methods(http.MethodPost, http.MethodOptions)
+		apiRouter := r.PathPrefix("/api").Subrouter()
+		apiRouter.HandleFunc("/auth/sign-in", auth.UserLoginEndpoint).Methods(http.MethodPost, http.MethodOptions)
+		apiRouter.HandleFunc("/auth/check-token", auth.CheckUserTokenEndpoint).Methods(http.MethodPost, http.MethodOptions)
 
-	adminRouter := apiRouter.PathPrefix("/").Subrouter()
-	adminRouter.Use(middleware.CorsMiddleware, middleware.CheckAdminRoute)
-	adminRouter.HandleFunc("/user", user.GetUsersEndpoint).Methods(http.MethodGet, http.MethodOptions)
-	adminRouter.HandleFunc("/user", user.CreateUserEndpoint).Methods(http.MethodPost, http.MethodOptions)
-	adminRouter.HandleFunc("/user", user.DeleteUserEndpoint).Methods(http.MethodDelete, http.MethodOptions)
-	adminRouter.HandleFunc("/user", user.UpdateUserEndpoint).Methods(http.MethodPut, http.MethodOptions)
-	adminRouter.HandleFunc("/auth/set-temporary-password", auth.SetTemporaryPasswordEndpoint).Methods(http.MethodPut, http.MethodOptions)
-	adminRouter.HandleFunc("/vpn", vpn.GetServerStatusEndpoint).Methods(http.MethodGet, http.MethodOptions)
-	adminRouter.HandleFunc("/vpn/operation", vpn.VpnOperationEndpoint).Methods(http.MethodPost, http.MethodOptions)
-	adminRouter.HandleFunc("/vpn/connections", vpn.GetActiveConnectionsEndpoint).Methods(http.MethodGet, http.MethodOptions)
-	adminRouter.HandleFunc("/groups", groups.GetGroupsEndpoint).Methods(http.MethodOptions, http.MethodGet)
+		adminRouter := apiRouter.PathPrefix("/").Subrouter()
+		adminRouter.Use(middleware.CorsMiddleware, middleware.CheckAdminRoute)
+		adminRouter.HandleFunc("/user", user.GetUsersEndpoint).Methods(http.MethodGet, http.MethodOptions)
+		adminRouter.HandleFunc("/user", user.CreateUserEndpoint).Methods(http.MethodPost, http.MethodOptions)
+		adminRouter.HandleFunc("/user", user.DeleteUserEndpoint).Methods(http.MethodDelete, http.MethodOptions)
+		adminRouter.HandleFunc("/user", user.UpdateUserEndpoint).Methods(http.MethodPut, http.MethodOptions)
+		adminRouter.HandleFunc("/auth/set-temporary-password", auth.SetTemporaryPasswordEndpoint).Methods(http.MethodPut, http.MethodOptions)
+		adminRouter.HandleFunc("/vpn", vpn.GetServerStatusEndpoint).Methods(http.MethodGet, http.MethodOptions)
+		adminRouter.HandleFunc("/vpn/operation", vpn.VpnOperationEndpoint).Methods(http.MethodPost, http.MethodOptions)
+		adminRouter.HandleFunc("/vpn/connections", vpn.GetActiveConnectionsEndpoint).Methods(http.MethodGet, http.MethodOptions)
+		adminRouter.HandleFunc("/groups", groups.GetGroupsEndpoint).Methods(http.MethodOptions, http.MethodGet)
 
-	userRouter := apiRouter.PathPrefix("/").Subrouter()
-	userRouter.Use(middleware.CorsMiddleware, middleware.CheckUserRoute)
-	userRouter.HandleFunc("/auth/change-password", auth.ChangeUserPasswordEndpoint).Methods(http.MethodPost, http.MethodOptions)
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/app", http.StatusSeeOther)
+		userRouter := apiRouter.PathPrefix("/").Subrouter()
+		userRouter.Use(middleware.CorsMiddleware, middleware.CheckUserRoute)
+		userRouter.HandleFunc("/auth/change-password", auth.ChangeUserPasswordEndpoint).Methods(http.MethodPost, http.MethodOptions)
+		r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/app", http.StatusSeeOther)
+		})
+		r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.FS(svelte))))
+
+		return r
+	}
+*/
+func setupRouter(service *auth.Service) *chi.Mux {
+	m := service.Middleware()
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/user", func(r chi.Router) {
+		r.Use(m.AdminOnly)
+		r.Get("/", user.GetUsersEndpoint)
+		r.Post("/", user.CreateUserEndpoint)
+		r.Delete("/", user.DeleteUserEndpoint)
+		r.Put("/", user.UpdateUserEndpoint)
 	})
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.FS(svelte))))
+
+	r.Route("/vpn", func(r chi.Router) {
+		r.Use(m.AdminOnly)
+		r.Get("/", vpn.GetServerStatusEndpoint)
+		r.Get("/connections", vpn.GetActiveConnectionsEndpoint)
+		r.Post("/", vpn.VpnOperationEndpoint)
+	})
+
+	r.Route("/group", func(r chi.Router) {
+		r.Use(m.AdminOnly)
+		r.Get("/", groups.GetGroupsEndpoint)
+		r.Get("/memberships", groups.GetGroupMembershipEndpoint)
+	})
+
+	authRoutes, avaRoutes := service.Handlers()
+	r.Mount("/auth", authRoutes)
+	r.Mount("/avatar", avaRoutes)
+
+	service.AddDirectProvider("local", provider.CredCheckerFunc(func(user, password string) (ok bool, err error) {
+		return true, err
+	}))
 
 	return r
 }
